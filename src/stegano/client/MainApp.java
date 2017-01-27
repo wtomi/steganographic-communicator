c34f2d36f59ead1e141d75e9fede98f2584459f8
wtomi <tomi.git@gmail.com>
2017-01-27 20:17:18 +0100

    Add main class MainApp

---
+++ src/stegano/client/MainApp.java
@@ -0,0 +1,95 @@
+package stegano.client;
+
+import javafx.collections.FXCollections;
+import javafx.scene.layout.AnchorPane;
+import stegano.client.model.Contact;
+import javafx.application.Application;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import stegano.client.view.ConversationsViewController;
+
+import java.io.IOException;
+
+/**
+ * Created by tommy on 27.01.2017.
+ */
+public class MainApp extends Application {
+
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+
+    private ObservableList<Contact> contacts = FXCollections.observableArrayList();
+
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("StenoClient");
+
+        contacts.add(new Contact("example 1"));
+        contacts.add(new Contact("example 2"));
+
+        initRootLayout();
+
+        showConversationsView();
+    }
+
+    /**
+     * Initializes the root layout.
+     */
+    public void initRootLayout() {
+        try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
+            rootLayout = (BorderPane) loader.load();
+
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Shows the contacts and conversation
+     */
+    public void showConversationsView() {
+        try {
+            // Load person overview.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/ConversationsView.fxml"));
+            AnchorPane personOverview = (AnchorPane) loader.load();
+
+            // Set person overview into the center of root layout.
+            rootLayout.setCenter(personOverview);
+
+            // Give the controller access to the main app.
+            ConversationsViewController controller = loader.getController();
+            controller.setMainApp(this);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Returns the main stage.
+     * @return
+     */
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public ObservableList<Contact> getContacts() {
+        return contacts;
+    }
+}
