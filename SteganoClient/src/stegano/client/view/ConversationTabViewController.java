ee13ff843fda40a1ecbda97d0187385ad5162f65
wtomi <tomi.git@gmail.com>
2017-02-20 02:10:39 +0100

    Add cellValue factory to List view and KeyEvent handler to TextArea

--- SteganoClient/src/stegano/client/view/ConversationTabViewController.java
+++ SteganoClient/src/stegano/client/view/ConversationTabViewController.java
@@ -1,8 +1,14 @@
 package stegano.client.view;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.control.Tab;
 import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyCode;
+import stegano.client.model.Message;
+
+import java.awt.event.KeyEvent;
 
 /**
  * Created by tommy on 19.02.2017.
@@ -16,11 +22,37 @@ public class ConversationTabViewController {
     @FXML
     private TextArea msgTextArea;
     @FXML
-    private ListView<String> conversationListView;
+    private ListView<Message> conversationListView;
+    @FXML
+    private Tab tab;
 
     @FXML
     private void initialize() {
-    }
 
+        msgTextArea.setOnKeyPressed(event -> {
+            if(event.getCode() == KeyCode.ENTER) {
+                String text = msgTextArea.getText();
 
+                msgTextArea.clear();
+            }
+        });
+
+        conversationListView.setCellFactory(listView -> new ListCell<Message>() {
+
+            @Override
+            protected void updateItem(Message item, boolean empty) {
+                super.updateItem(item, empty);
+                    setText(item.getMessage());
+                    if(item.getAuthor() == Message.Author.ME) {
+                        setStyle("-fx-alignment: CENTER-RIGHT;" +
+                                "-fx-background-color: lightcyan;");
+                    }
+                    else {
+                        setStyle("-fx-alignment: CENTER-LEFT;" +
+                                "-fx-background-color: lightgrey");
+                    }
+            }
+        });
+
+        }
 }
