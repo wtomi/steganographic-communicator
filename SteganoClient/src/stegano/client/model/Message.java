eb2175ea0a049f2de9b968e4a36922795d20dc0c
wtomi <tomi.git@gmail.com>
2017-02-20 02:08:22 +0100

    Add info about author to Message

--- SteganoClient/src/stegano/client/model/Message.java
+++ SteganoClient/src/stegano/client/model/Message.java
@@ -1,20 +1,44 @@
 package stegano.client.model;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 /**
  * Created by tommy on 19.02.2017.
  */
 public class Message {
-    private String msgText;
 
-    public Message(String msgText) {
-        this.msgText = msgText;
+    public enum Author{
+        ME,
+        NOT_ME
     }
 
-    public String getMsgText() {
-        return msgText;
+    private StringProperty message;
+    private Author author;
+
+    public Message(String msgText, Author author) {
+        message = new SimpleStringProperty((msgText));
+        this.author = author;
     }
 
-    public void setMsgText(String msgText) {
-        this.msgText = msgText;
+    public String getMessage() {
+       return message.get();
     }
+
+    public void setMessage(String msgText) {
+        message.set(msgText);
+    }
+
+    public StringProperty getMessageProperty() {
+        return message;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
 }
